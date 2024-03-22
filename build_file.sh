d62f481db712e25c76494fdcf6e2b6d23fc15781
Ahsan280 <ahsanrehman281@gmail.com>
2024-03-22 16:05:29 +0500

    static and media included_2

--- build_file.sh
+++ build_file.sh
@@ -1,2 +1,5 @@
+cd G:\Django Ecommerce\ecommerce\static
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+exit 0
+
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+absolute_path = os.path.abspath("ecommerce/static")
+print(absolute_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
