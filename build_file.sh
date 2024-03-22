c8d0290643b8702a35784cd36eda09157320e30f
Ahsan280 <ahsanrehman281@gmail.com>
2024-03-22 16:30:24 +0500

    static and media included_3

--- build_file.sh
+++ build_file.sh
@@ -1,5 +1,5 @@
-cd G:\Django Ecommerce\ecommerce\static
+
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-exit 0
+
 
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-absolute_path = os.path.abspath("ecommerce/static")
-print(absolute_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,10 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR /'static'
-STATICFILES_DIRS=[
-    'ecommerce/static',
-]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR /'media'
